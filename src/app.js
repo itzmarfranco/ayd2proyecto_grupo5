79c4734d7d93bfe3d4a9f0c93e28c942d5699645
EverChicas <everchicasp@gmail.com>
2020-05-08 19:50:31 -0600

    correccion credenciales de bd

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const myConnection = require('express-myconnection');
 
 const app = express();
 
+const hostDB = '18.188.212.217';
+const userDB = 'reminder';
+const passDB = '#Reminder1S2020';
+const nameDB = 'REMINDER';
+const portDB = '3306';
 
 //importing routes
 const gameRoutes = require('./routes/rutas');
@@ -13,7 +18,7 @@ const gameRoutes = require('./routes/rutas');
 
 
 //settings
-app.set('port',process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -23,17 +28,17 @@ app.use(
   myConnection(
     mysql,
     {
-      host: "localhost",
-      user: "root",
-      password: "1234",
-      port: 3306,
-      database: "proyecto",
+      host: `${hostDB}`,
+      user: `${userDB}`,
+      password: `${passDB}`,
+      database: `${nameDB}`,
+      port: `${portDB}`
     },
     "single"
   )
 );
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.use('/', gameRoutes);
@@ -44,6 +49,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.listen(app.get('port'), ()=>{
-	console.log('Server on port 3000');
+app.listen(app.get('port'), () => {
+  console.log('Server on port 3000');
 });
